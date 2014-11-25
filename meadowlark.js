4352e29e393982fcc951c3ebbd8cdd00ff7fe635
lorenae <lorenae@gmail.com>
2014-11-24 23:10:57 -0200

    Initial release

---
+++ meadowlark.js
@@ -0,0 +1,30 @@
+//meadowlark.js
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.get('/', function(req, res){
+	res.type('text/plain');
+	res.send('Meadowlark Travel');
+});
+app.get('/about', function(req, res){
+	res.type('text/plain');
+	res.send('About Meadowlark Travel');
+});
+// custom 404 page
+app.use(function(req, res, next){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on http://localhost:' +
+	app.get('port') + '; press Ctrl-C to terminate.' );
+});
