98b4cd42e22b5d86c15feba39ee25277f9d80a4b
lorenae <lorenae@gmail.com>
2014-11-25 00:27:19 -0200

    Initial version

--- meadowlark.js
+++ meadowlark.js
@@ -1,29 +1,40 @@
 //meadowlark.js
-var express = require('express');
-var app = express();
-app.set('port', process.env.PORT || 3000);
-app.get('/', function(req, res){
-	res.type('text/plain');
-	res.send('Meadowlark Travel');
-});
-app.get('/about', function(req, res){
-	res.type('text/plain');
-	res.send('About Meadowlark Travel');
-});
-// custom 404 page
-app.use(function(req, res, next){
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not Found');
-});
 
-// custom 500 page
+var express = require('express');
+var fortune = require('./lib/fortune.js');
+
+
+var app = express();
+// set up handlebars view engine
+var handlebars = require('express-handlebars')
+.create({ defaultLayout:'main' });
+	app.engine('handlebars', handlebars.engine);
+	app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/about', function(req, res) {
+	res.render('about', { fort: fortune.getFortune() } );
+});
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
+
+
+
 app.listen(app.get('port'), function(){
 	console.log( 'Express started on http://localhost:' +
 	app.get('port') + '; press Ctrl-C to terminate.' );
